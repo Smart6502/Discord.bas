0a7bc35c5aec4e8aac88939c5982b5e71bdf21de
PQCraft <58464017+PQCraft@users.noreply.github.com>
2021-10-29 08:31:42 -0400

    Update exec.js

--- commands/exec.js
+++ commands/exec.js
@@ -44,7 +44,7 @@ module.exports.cmd = async (client, message, _args) => {
     });
     setTimeout(() => {
         if (clibasic_process.exitCode === null) {
-            clibasic_process.kill(9);
+            clibasic_process.kill('SIGKILL');
             if (!output || output.trim() === "") {output = "\u200B\n";}
             if (output.length > 800) {output = "...\n" + output.substr(output.length - 800, output.length - 1);}
             outputEmbed.setColor(client.config.embeds.error_color).addFields({ name: 'Output', value: `\`\`\`\n${output}\n\`\`\`__${" ".repeat(34)}   __\n` },).setFooter(`Killed after ${(client.config.maxExecTime ? client.config.maxExecTime : 10000) / 1000} second(s).`);
