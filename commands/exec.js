83ea558e75ac46965c889902fbf88d4a77deb184
PQCraft <58464017+PQCraft@users.noreply.github.com>
2021-10-28 21:44:49 -0400

    Update exec.js

--- commands/exec.js
+++ commands/exec.js
@@ -38,7 +38,7 @@ module.exports.cmd = async (client, message, _args) => {
         const outputEmbed = new MessageEmbed()
             .setColor('#1E11E1')
             .addFields(
-                { name: 'Output', value: `\`\`\`\n${output}\n\`\`\`__${" ".repeat(38)}__\n` },
+                { name: 'Output', value: `\`\`\`\n${output}\n\`\`\`__${" ".repeat(34)}    __\n` },
             )
             .setFooter(`Executed in ${(Date.now() - start_time) / 1000} seconds with exit code ${code}.`);
         executing_msg.edit(`Done. `);
