57d0219469b3a7e57a9f0f6f3bad256f44906204
PQCraft <58464017+PQCraft@users.noreply.github.com>
2021-10-29 22:35:07 -0400

    Delete hi.js

--- commands/hi.js
+++
@@ -1,9 +0,0 @@
-module.exports.cmd = (_client, message, _args) => {
-    message.reply("hi!");
-}
-
-module.exports.help = {
-    name: "hi",
-    description: "replies with 'hi!'",
-    usage: "%PREFIX%hi"
-}
